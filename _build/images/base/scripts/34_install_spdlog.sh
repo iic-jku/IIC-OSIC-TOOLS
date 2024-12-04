da68693f67899151d6d71ad7d5e784783176b71b
Harald Pretl <harald.pretl@jku.at>
2024-12-04 12:05:00 +0100

    Build custom SPDLOG version for OpenROAD (crash when using 1.9.x)

---
+++ _build/images/base/scripts/34_install_spdlog.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+# Install syslog (dependency of OpenROAD)
+SPDLOG_PREFIX="/usr/local"
+SPDLOG_VERSION=1.8.1
+echo "[INFO] Installing SPDLOG version $SPDLOG_VERSION into $SPDLOG_PREFIX"
+cd /tmp || exit 1
+git clone --depth=1 -b "v${SPDLOG_VERSION}" https://github.com/gabime/spdlog.git
+cd spdlog || exit 1
+cmake -DCMAKE_INSTALL_PREFIX="${SPDLOG_PREFIX}" -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DSPDLOG_BUILD_EXAMPLE=OFF -B build .
+cmake --build build -j "$(nproc)" --target install
+rm -rf /tmp/*
