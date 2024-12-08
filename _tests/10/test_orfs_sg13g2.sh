1307ee73776349ca06ad717c77d577096f03b224
Harald Pretl <harald.pretl@jku.at>
2024-12-08 13:55:11 +0100

    Adding new test for ORFS with SG13G2

---
+++ _tests/10/test_orfs_sg13g2.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2024 Harald Pretl
+# Johannes Kepler University, Institute for Integrated Circuits
+# SPDX-License-Identifier: Apache-2.0
+#
+# Test if the OpenROAD flow scripts (ORFS) run successfully; we run
+# this for IHP SG13G2 only
+
+RESULT=/tmp/result_orfs_sg13g2.log
+WORK_DIR=/tmp/orfs_sg13g2
+FLOW_HOME=$WORK_DIR/orfs/flow
+
+mkdir -p $WORK_DIR && cd $WORK_DIR || exit 1
+git clone --quiet --depth=1 https://github.com/The-OpenROAD-Project/OpenROAD-flow-scripts.git orfs
+cd $FLOW_HOME || exit 1
+
+# prepare environment for ORFS
+export YOSYS_EXE=$TOOLS/yosys/bin/yosys
+export OPENROAD_EXE=$TOOLS/openroad-latest/bin/openroad
+export OPENSTA_EXE=$TOOLS/openroad-latest/bin/opensta
+export FLOW_HOME
+
+# FIXME this is need to run w/o errors
+export GDS_ALLOW_EMPTY=spi_DEF_FILL
+
+# run ORFS with IHP130 SG13G2
+export DESIGN_CONFIG=./designs/ihp-sg13g2/spi/config.mk
+make > $RESULT
+
+# check if there is an error in the log
+if grep -q "ERROR" "$RESULT"; then
+    echo "[ERROR] Test <ORFS with sg13g2> FAILED."
+    exit 1
+else
+    echo "[INFO] Test <ORFS with sg13g2> passed."
+    exit 0
+fi
