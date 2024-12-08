6f69c6312202c06042ffde0aa5022d52f6d94973
Harald Pretl <harald.pretl@jku.at>
2024-12-08 11:15:27 +0100

    Build bottleneck locally to fix error on aarch64

--- _build/images/base/scripts/90_basepkg_install.sh
+++ _build/images/base/scripts/90_basepkg_install.sh
@@ -9,17 +9,16 @@ pip3 install --upgrade --no-cache-dir \
 	jupyter-collaboration \
 	jupyterlab-night
 
-#FIXME gdfsfactory >= 8.8.6 requires Python 3.11
 echo "[INFO] Install EDA packages via PIP"
 pip3 install --upgrade --no-cache-dir \
 	amaranth==0.5.3 \
-	cace==2.5.3 \
+	cace==2.5.4 \
 	cocotb==1.9.2 \
 	edalize==0.6.0 \
 	fusesoc==2.4 \
-	gdsfactory==8.19.5 \
+	gdsfactory==8.23.0 \
 	gdspy==1.6.13 \
-	openlane==2.2.5 \
+	openlane==2.2.9 \
 	openram==1.2.48 \
 	pygmid==1.2.12 \
 	pyrtl==0.11.2 \
@@ -27,8 +26,8 @@ pip3 install --upgrade --no-cache-dir \
 	pyuvm==3.0.0 \
 	pyverilog==1.3.0 \
 	schemdraw[svgmath]==0.19 \
-	scikit-rf==1.4.1 \
-	siliconcompiler==0.28.8 \
+	scikit-rf==1.5.0 \
+	siliconcompiler==0.28.9 \
 	spyci==1.0.2 \
 	volare==0.19.2
 
---
+++ _build/images/base/scripts/91_install_bottleneck.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+# Need to compile Bottleneck manually, as otherwise
+# error in gdsfactory and scikit-rf on aarch64
+cd /tmp
+git clone --depth=1 https://github.com/pydata/bottleneck.git
+cd bottleneck || exit 1
+pip install .
