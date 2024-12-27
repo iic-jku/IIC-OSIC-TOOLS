28c2a2dd76584c4fb51734b6eca18ceae9cf2839
Harald Pretl <harald@pretl.eu>
2024-12-27 16:15:56 +0100

    Remove FIXME (these changes are here to stay)

--- _build/images/iic-osic-tools/skel/dockerstartup/scripts/env.sh
+++ _build/images/iic-osic-tools/skel/dockerstartup/scripts/env.sh
@@ -122,11 +122,11 @@ export PDKPATH=$PDK_ROOT/$PDK
 export STD_CELL_LIBRARY=sky130_fd_sc_hd
 export SPICE_USERINIT_DIR=$PDK_ROOT/$PDK/libs.tech/ngspice
 
-# FIXME this gets rid of a few libGL errors
+# this gets rid of a few libGL errors
 # https://unix.stackexchange.com/questions/589236/libgl-error-no-matching-fbconfigs-or-visuals-found-glxgears-error-docker-cu
 export LIBGL_ALWAYS_INDIRECT=1
 
-# FIXME this gets rid of the DBUS warning
+# this gets rid of the DBUS warning
 # https://unix.stackexchange.com/questions/230238/x-applications-warn-couldnt-connect-to-accessibility-bus-on-stderr/230442#230442
 export NO_AT_BRIDGE=1
 
