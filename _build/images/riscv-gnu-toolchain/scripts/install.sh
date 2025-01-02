954e01b32d24bdfc68d6c3068266a83ae23fbe17
Georg Zachl <it@zachlge.org>
2025-01-02 17:22:12 +0100

    Strip riscv-toolchain binaries. (Saves another 600-700Mb about).

--- _build/images/riscv-gnu-toolchain/scripts/install.sh
+++ _build/images/riscv-gnu-toolchain/scripts/install.sh
@@ -9,3 +9,5 @@ git checkout "$RISCV_GNU_TOOLCHAIN_REPO_COMMIT"
 mkdir build && cd build
 ../configure --prefix="${TOOLS}/$RISCV_GNU_TOOLCHAIN_NAME" --enable-multilib --with-sim=spike
 make ASFLAGS=g0 CFLAGS=-g0 CXXFLAGS=-g0 -j"$(nproc)" 
+
+find "${TOOLS}/$RISCV_GNU_TOOLCHAIN_NAME/bin" -type f -executable -exec strip {} \;
