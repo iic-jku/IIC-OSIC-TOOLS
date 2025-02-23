4c03da76af776f74e98a77ed1137f44e93eed4a4
Harald Pretl <harald@pretl.eu>
2025-02-23 01:02:00 +0100

    Bumping tool versions

--- _build/images/base/scripts/90_basepkg_install.sh
+++ _build/images/base/scripts/90_basepkg_install.sh
@@ -16,11 +16,11 @@ pip3 install --upgrade --no-cache-dir --break-system-packages --ignore-installed
 	cocotb==1.9.2 \
 	edalize==0.6.0 \
 	fusesoc==2.4.2 \
-	gdsfactory==8.32.1 \
+	gdsfactory==9.0.1 \
 	gdspy==1.6.13 \
 	klayout-pex==0.1.13 \
 	lctime==0.0.24 \
-	openlane==2.3.6 \
+	openlane==2.3.10 \
 	openram==1.2.48 \
 	pygmid==1.2.12 \
 	pyrtl==0.11.2 \
@@ -29,7 +29,7 @@ pip3 install --upgrade --no-cache-dir --break-system-packages --ignore-installed
 	pyverilog==1.3.0 \
 	schemdraw[svgmath]==0.19 \
 	scikit-rf==1.5.0 \
-	siliconcompiler==0.29.4 \
+	siliconcompiler==0.30.0 \
 	spicelib==1.4.0 \
 	spyci==1.0.2 \
 	volare==0.20.6
