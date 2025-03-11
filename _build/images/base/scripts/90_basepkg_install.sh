0a9ff52f0fbbcd39c0375a35b181b87f4031d028
Harald Pretl <harald@pretl.eu>
2025-03-11 19:14:44 +0100

    Bump tool versions

--- _build/images/base/scripts/90_basepkg_install.sh
+++ _build/images/base/scripts/90_basepkg_install.sh
@@ -29,7 +29,7 @@ pip3 install --upgrade --no-cache-dir --break-system-packages --ignore-installed
 	pyverilog==1.3.0 \
 	schemdraw[svgmath]==0.20 \
 	scikit-rf==1.6.2 \
-	siliconcompiler==0.31.1 \
+	siliconcompiler==0.32.0 \
 	spicelib==1.4.2 \
 	spyci==1.0.2 \
 	volare==0.20.6
