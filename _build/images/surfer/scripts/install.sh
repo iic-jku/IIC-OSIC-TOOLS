c0a422af5c93f646267b4593cd0eb2b8b513d39a
Harald Pretl <harald@pretl.eu>
2025-04-04 08:35:19 +0200

    Fix build script for surfer

--- _build/images/surfer/scripts/install.sh
+++ _build/images/surfer/scripts/install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
 cd /tmp || exit 1
-
 export RUSTUP_HOME=/tmp/rustup
 export CARGO_HOME=/tmp/cargo
 export PATH=$CARGO_HOME/bin:$PATH
 rustup default stable
 
-cargo update
-cargo install --git ${SURFER_REPO_URL} --tag ${SURFER_REPO_COMMIT} surfer --root "${TOOLS}/${SURFER_NAME}" --locked
+git clone --depth=1 "${SURFER_REPO_URL}" "${SURFER_NAME}"
+cd "${SURFER_NAME}" || exit 1
+git checkout "${SURFER_REPO_COMMIT}"
+git submodule update --init --recursive
+cargo install surfer --root "${TOOLS}/${SURFER_NAME}" --locked
