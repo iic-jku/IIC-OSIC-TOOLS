845c3955afdc69bfa481ba3ca5af9c6d87d97023
Harald Pretl <harald@pretl.eu>
2025-04-25 13:49:25 +0200

    Add check that `openlane` is available

--- _tests/04/test_openlane_gf180mcud.sh
+++ _tests/04/test_openlane_gf180mcud.sh
@@ -9,21 +9,23 @@
 #
 # We do this only for gf180mcuD for now.
 
-RESULT=/tmp/result_ol_gf180mcud.log
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+if command -v openlane >/dev/null 2>&1; then
+    RESULT=/tmp/result_ol_gf180mcud.log
+    DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# Switch to gf180mcuD PDK
-# shellcheck source=/dev/null
-source iic-pdk-script.sh gf180mcuD gf180mcu_fd_sc_mcu7t5v0 > /dev/null
-# Run the OpenLane2 smoke test
-mkdir -p /tmp/ol2_gf180mcu
-cp $DIR/counter.* /tmp/ol2_gf180mcu
-openlane --manual-pdk /tmp/ol2_gf180mcu/counter.json > $RESULT
-# Check if there is an error in the log
-if grep -q "ERROR" "$RESULT"; then
-    echo "[ERROR] Test <OpenLane smoke-test with gf180mcuD> FAILED."
-    exit 1
-else
-    echo "[INFO] Test <OpenLane smoke-test with gf180mcuD> passed."
-    exit 0
+    # Switch to gf180mcuD PDK
+    # shellcheck source=/dev/null
+    source iic-pdk-script.sh gf180mcuD gf180mcu_fd_sc_mcu7t5v0 > /dev/null
+    # Run the OpenLane2 smoke test
+    mkdir -p /tmp/ol2_gf180mcu
+    cp "$DIR"/counter.* /tmp/ol2_gf180mcu
+    openlane --manual-pdk /tmp/ol2_gf180mcu/counter.json > $RESULT
+    # Check if there is an error in the log
+    if grep -q "ERROR" "$RESULT"; then
+        echo "[ERROR] Test <OpenLane smoke-test with gf180mcuD> FAILED."
+        exit 1
+    else
+        echo "[INFO] Test <OpenLane smoke-test with gf180mcuD> passed."
+        exit 0
+    fi
 fi
