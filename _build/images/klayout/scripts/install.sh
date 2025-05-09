f638568b3be727bdf1bc280e7224d4f8ae68096c
Harald Pretl <harald@pretl.eu>
2025-05-09 10:07:53 +0200

    Build KLayout incl. Qt-bindings

--- _build/images/klayout/scripts/install.sh
+++ _build/images/klayout/scripts/install.sh
@@ -7,4 +7,7 @@ cd "${KLAYOUT_NAME}" || exit 1
 git checkout "${KLAYOUT_REPO_COMMIT}"
 prefix=${TOOLS}/${KLAYOUT_NAME}
 mkdir -p "$prefix"
-./build.sh -j"$(nproc)" -prefix "$prefix" -without-qtbinding
+#./build.sh -j"$(nproc)" -prefix "$prefix" -without-qtbinding
+# we add the Qt-bindings again as it is needed for DRC and LVS, see
+# https://github.com/iic-jku/IIC-OSIC-TOOLS/issues/111
+./build.sh -j"$(nproc)" -prefix "$prefix"
