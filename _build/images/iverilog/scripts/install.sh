a0e6f0cde11d0fad97aaf762fb18c9aa16b8a1cb
Harald Pretl <harald.pretl@jku.at>
2025-05-19 17:42:45 +0200

    Add configure flag for libvvp

--- _build/images/iverilog/scripts/install.sh
+++ _build/images/iverilog/scripts/install.sh
@@ -7,6 +7,6 @@ cd "${IVERILOG_NAME}" || exit 1
 git checkout "${IVERILOG_REPO_COMMIT}"
 chmod +x autoconf.sh
 ./autoconf.sh
-./configure --prefix="${TOOLS}/${IVERILOG_NAME}"
+./configure --prefix="${TOOLS}/${IVERILOG_NAME}" --enable-libvvp
 make -j"$(nproc)"
 make install
