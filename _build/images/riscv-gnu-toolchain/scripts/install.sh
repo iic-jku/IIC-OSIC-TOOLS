31458d80d372ca02b794ace109e081b187a00c5c
Harald Pretl <harald.pretl@jku.at>
2025-06-10 14:04:56 +0900

    Remove old WA, seems not needed any longer

--- _build/images/riscv-gnu-toolchain/scripts/install.sh
+++ _build/images/riscv-gnu-toolchain/scripts/install.sh
@@ -5,13 +5,10 @@ cd /tmp || exit 1
 git clone --filter=blob:none "$RISCV_GNU_TOOLCHAIN_REPO_URL" "$RISCV_GNU_TOOLCHAIN_NAME"
 cd "$RISCV_GNU_TOOLCHAIN_NAME" || exit 1
 git checkout "$RISCV_GNU_TOOLCHAIN_REPO_COMMIT"
-#FIXME WA according to https://github.com/riscv-collab/riscv-gnu-toolchain/issues/1669
-sed -i '/shallow = true/d' .gitmodules
-sed -i 's/--depth 1//g' Makefile.in
-
-#git submodule update --init --recursive
+#FIXME git submodule update --init --recursive
 mkdir build && cd build
-../configure --prefix="${TOOLS}/$RISCV_GNU_TOOLCHAIN_NAME" --enable-multilib --with-sim=spike
+../configure --enable-multilib --with-sim=spike --prefix="${TOOLS}/$RISCV_GNU_TOOLCHAIN_NAME" 
+# these make flags remove the debug symbols
 make ASFLAGS=g0 CFLAGS=-g0 CXXFLAGS=-g0 -j"$(nproc)" 
-
+# and we strip the binaries to reduce size
 find "${TOOLS}/$RISCV_GNU_TOOLCHAIN_NAME/bin" -type f -executable -exec strip {} \;
