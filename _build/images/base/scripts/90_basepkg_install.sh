d2e9c913656d20a0064732e13f2d01d7b79e2db6
Harald Pretl <harald.pretl@jku.at>
2025-06-17 12:09:57 +0200

    Bump tool versions

--- _build/images/base/scripts/90_basepkg_install.sh
+++ _build/images/base/scripts/90_basepkg_install.sh
@@ -19,11 +19,11 @@ pip3 install --upgrade --no-cache-dir --break-system-packages --ignore-installed
 	fusesoc==2.4.3 \
 	gdsfactory==9.8.0 \
 	gdspy==1.6.13 \
-	lctime==0.0.24 \
+	lctime==0.0.25 \
 	klayout-pex==0.2.5 \
 	openram==1.2.48 \
 	pygmid==1.2.12 \
-	pyrtl==0.11.2 \
+	pyrtl==0.11.3 \
 	pyspice==1.5 \
 	pyuvm==3.0.0 \
 	pyverilog==1.3.0 \
