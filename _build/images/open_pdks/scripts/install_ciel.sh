18d16727717feb32afff7cdb069373da71cd5232
Harald Pretl <harald.pretl@jku.at>
2025-06-20 17:43:49 +0200

    Remove gf180mcuC (to save image size)

--- _build/images/open_pdks/scripts/install_ciel.sh
+++ _build/images/open_pdks/scripts/install_ciel.sh
@@ -15,43 +15,16 @@ echo "[INFO] Installing GF180 PDK."
 # FIXME: use common tag from Dockerfile.
 ciel enable f2e289da6753f26157a308c492cf990fdcd4932d --pdk-family gf180mcu
 
-# remove gf180mcuA and gf180mcuB for size reasons
+# remove gf180mcuA, gf180mcuB and gf180mcuC for size reasons
 rm -rf "$PDK_ROOT"/ciel/gf180mcu/versions/*/gf180mcuA
 rm -rf "$PDK_ROOT"/ciel/gf180mcu/versions/*/gf180mcuB
+rm -rf "$PDK_ROOT"/ciel/gf180mcu/versions/*/gf180mcuC
 rm -rf "$PDK_ROOT"/gf180mcuA
 rm -rf "$PDK_ROOT"/gf180mcuB
+rm -rf "$PDK_ROOT"/gf180mcuC
 
 git clone https://github.com/martinjankoehler/globalfoundries-pdk-libs-gf180mcu_fd_pr.git --branch gdsfactory-v7-to-v9-port /tmp/glofo-mjk
 git clone https://github.com/mabrains/globalfoundries-pdk-libs-gf180mcu_fd_pr.git /tmp/glofo-mabrains
-
-if [ -d "$PDK_ROOT/gf180mcuC" ]; then
-	#FIXME gzip Liberty (.lib) files
-	#FIXME cd "$PDK_ROOT/gf180mcuC/libs.ref"
-	#FIXME find . -name "*.lib" -exec gzip {} \;
-
-	cd "$PDK_ROOT/gf180mcuC/libs.tech/ngspice" || exit 1
-	
-	# setup empty .spiceinit (harmonize with SG13G2)
-	touch .spiceinit
-
-	# remove testing folders to save space
-	cd "$PDK_ROOT/gf180mcuC"
-	find . -name "testing" -print0 | xargs -0 rm -rf
-
-	# fix test schematic relative paths
-	sed -i 's/{test_/{tests\/test_/g' $PDK_ROOT/gf180mcuC/libs.tech/xschem/tests/0_top.sch
-
-	# fix missing PDK variant in path definitions for in xschemrc
-	sed -i 's|set 180MCU_MODELS ${PDK_ROOT}/models/ngspice|set 180MCU_MODELS ${PDK_ROOT}/gf180mcuC/libs.tech/ngspice|' $PDK_ROOT/gf180mcuC/libs.tech/xschem/xschemrc
-
-	# Replace pymacro with working pcells.
-	rm -rf $PDK_ROOT/gf180mcuC/libs.tech/klayout/tech/pymacros
-	cp -a /tmp/glofo-mjk/cells/klayout/pymacros $PDK_ROOT/gf180mcuC/libs.tech/klayout/tech/pymacros
-
-	cp -r /tmp/glofo-mabrains/rules/klayout/macros/ $PDK_ROOT/gf180mcuC/libs.tech/klayout/
-	chmod -R 777 $PDK_ROOT/gf180mcuC/libs.tech/klayout/macros
-
-fi
 
 if [ -d "$PDK_ROOT/gf180mcuD" ]; then
 	#FIXME gzip Liberty (.lib) files
