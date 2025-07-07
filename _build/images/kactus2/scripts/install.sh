91d92bcf99ef8b8a81420abbc878b22f945920d8
Harald Pretl <harald.pretl@jku.at>
2025-07-07 13:34:05 +0200

    Fixing Kactus2 install

--- _build/images/kactus2/scripts/install.sh
+++ _build/images/kactus2/scripts/install.sh
@@ -5,6 +5,7 @@ cd /tmp || exit 1
 git clone --filter=blob:none "${KACTUS_REPO_URL}" "${KACTUS_NAME}"
 cd "${KACTUS_NAME}" || exit 1
 git checkout "${KACTUS_REPO_COMMIT}"
-./configure --prefix="${TOOLS}/${KACTUS_NAME}"
+sed -i "s|^LOCAL_INSTALL_DIR=\".*\"|LOCAL_INSTALL_DIR=\"${TOOLS}/${KACTUS_NAME}\"|" .qmake.conf
+./configure
 make -j"$(nproc)"
 make install
