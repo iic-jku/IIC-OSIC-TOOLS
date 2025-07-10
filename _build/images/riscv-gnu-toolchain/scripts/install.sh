ec5f6ca99ebc2eaf54ee499023899adbede4ab84
Harald Pretl <harald.pretl@jku.at>
2025-07-10 12:19:38 +0200

    Update riscv-gnu-toolchain install script
    Removed the '--with-sim=spike' option from the configure command and updated the strip command to process all files under the toolchain directory, not just the bin directory.

--- _build/images/riscv-gnu-toolchain/scripts/install.sh
+++ _build/images/riscv-gnu-toolchain/scripts/install.sh
@@ -5,10 +5,9 @@ cd /tmp || exit 1
 git clone --filter=blob:none "$RISCV_GNU_TOOLCHAIN_REPO_URL" "$RISCV_GNU_TOOLCHAIN_NAME"
 cd "$RISCV_GNU_TOOLCHAIN_NAME" || exit 1
 git checkout "$RISCV_GNU_TOOLCHAIN_REPO_COMMIT"
-#FIXME git submodule update --init --recursive
 mkdir build && cd build
-../configure --enable-multilib --with-sim=spike --prefix="${TOOLS}/$RISCV_GNU_TOOLCHAIN_NAME" 
+../configure --enable-multilib --prefix="${TOOLS}/$RISCV_GNU_TOOLCHAIN_NAME" 
 # these make flags remove the debug symbols
 make ASFLAGS=g0 CFLAGS=-g0 CXXFLAGS=-g0 -j"$(nproc)" 
 # and we strip the binaries to reduce size
-find "${TOOLS}/$RISCV_GNU_TOOLCHAIN_NAME/bin" -type f -executable -exec strip {} \;
+find "${TOOLS}/$RISCV_GNU_TOOLCHAIN_NAME" -type f -executable -exec strip {} \;
