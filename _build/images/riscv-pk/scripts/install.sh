6b93d80176726bb926cad88c19826ddf09755412
Harald Pretl <harald@pretl.eu>
2025-07-12 14:44:42 +0200

    Revert "Optimize riscv-pk build for size and add multilib"
    This reverts commit b741dd596dd252251c60ab34d934b06cb1c1ceeb.

--- _build/images/riscv-pk/scripts/install.sh
+++ _build/images/riscv-pk/scripts/install.sh
@@ -8,18 +8,8 @@ git clone --filter=blob:none "${RISCV_PK_REPO_URL}" "${RISCV_PK_NAME}"
 cd "${RISCV_PK_NAME}" || exit 1
 git checkout "${RISCV_PK_REPO_COMMIT}"
 mkdir build && cd build
-
-../configure --prefix="$RISCV" \
-    --host=riscv64-unknown-elf \
-    --with-arch=rv64gc_zifencei \
-    --with-multilib-generator="rv64gc-lp64d--;rv32i-ilp32--;rv32e-ilp32e--;rv32imcb-ilp32--"
-
-make -j"$(nproc)" \
-    ASFLAGS="-Os -g0" \
-    CFLAGS="-Os -g0" \
-    CXXFLAGS="-Os -g0" \
-    LDFLAGS="-Wl,-s"
-
+../configure --prefix="$RISCV" --host=riscv64-unknown-elf --with-arch=rv64gc_zifencei
+make -j"$(nproc)"
 make install
 # and we strip the binaries to reduce size
 find "$RISCV" -type f -executable -exec strip {} \;
