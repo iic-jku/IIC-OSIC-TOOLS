2dc094913dc46433ef209330f4479217f036b5ae
Harald Pretl <harald@pretl.eu>
2025-07-12 14:48:47 +0200

    Optimize RISC-V toolchain build for size

--- _build/images/riscv-gnu-toolchain/scripts/install.sh
+++ _build/images/riscv-gnu-toolchain/scripts/install.sh
@@ -6,8 +6,18 @@ git clone --filter=blob:none "$RISCV_GNU_TOOLCHAIN_REPO_URL" "$RISCV_GNU_TOOLCHA
 cd "$RISCV_GNU_TOOLCHAIN_NAME" || exit 1
 git checkout "$RISCV_GNU_TOOLCHAIN_REPO_COMMIT"
 mkdir build && cd build
-../configure --enable-multilib --prefix="${TOOLS}/$RISCV_GNU_TOOLCHAIN_NAME" 
-# these make flags remove the debug symbols
-make ASFLAGS=g0 CFLAGS=-g0 CXXFLAGS=-g0 -j"$(nproc)" 
+
+../configure \
+    --enable-multilib \
+    --with-multilib-generator="rv64gc-lp64d--;rv32i-ilp32--;rv32e-ilp32e--;rv32imcb-ilp32--" \
+    --prefix="${TOOLS}/$RISCV_GNU_TOOLCHAIN_NAME" 
+
+make \
+ ASFLAGS="-Os -g0" \
+ CFLAGS="-Os -g0" \
+ CXXFLAGS="-Os -g0" \
+ LDFLAGS="-Wl,-s" \
+ -j"$(nproc)" 
+
 # and we strip the binaries to reduce size
 find "${TOOLS}/$RISCV_GNU_TOOLCHAIN_NAME" -type f -executable -exec strip {} \;
