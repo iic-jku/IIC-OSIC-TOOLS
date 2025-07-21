199683368f0aed115bde87cf26c3dfe65b480a81
Georg Zachl <it@zachlge.org>
2025-07-21 19:05:19 +0200

    Rework test script. Now, tests actually run in parallel. The parameter now requires the full image tag to be selected (makes it more flexible).

--- _tests/run_docker_tests.sh
+++ _tests/run_docker_tests.sh
@@ -6,23 +6,23 @@
 # Run all tests (checks) in the subdirectories using a specified Docker image.
 
 if [ $# -ne 1 ]; then
-    echo "[ERROR] Please specify the image tag to test!"
+    echo "[ERROR] Please specify the full image tag to test! (e.G.: hpretl/iic-osic-tools:latest)"
     exit 1
 fi
 
-TAG=$1
+FULL_TAG=$1
 RAND=$(hexdump -e '/1 "%02x"' -n4 < /dev/urandom)
 CONTAINER_NAME=iic-osic-tools_test${RAND}
 CMD=_run_tests_${RAND}.sh
 WORKDIR=/foss/designs
 
 # Check if newer image is available and pull if needed
-docker pull --quiet hpretl/iic-osic-tools:"$TAG" > /dev/null
+docker pull --quiet "$FULL_TAG" > /dev/null
 
 # Create the test runner script
 cat <<EOL > "$CMD"
 #!/bin/bash
-find $WORKDIR -type f -name "test*.sh" -exec parallel --halt soon,fail=1 ::: {} \;
+find $WORKDIR -type f -name "test*.sh" -exec parallel --halt soon,fail=1 ::: {} +
 if [ \$? -ne 0 ]; then
     echo "------------------------------------"
     echo "[ERROR] AT LEAST ONE TEST FAILED :-("
@@ -38,7 +38,7 @@ EOL
 chmod +x "$CMD"
 
 # Now run the actual tests
-docker run -it --rm --name "$CONTAINER_NAME" --user "$(id -u):$(id -g)" -e DISPLAY= -v "$PWD":$WORKDIR:rw hpretl/iic-osic-tools:"$TAG" -s "$WORKDIR/$CMD"
+docker run -it --rm --name "$CONTAINER_NAME" --user "$(id -u):$(id -g)" -e DISPLAY= -v "$PWD":$WORKDIR:rw "$FULL_TAG" -s "$WORKDIR/$CMD"
 
 # Cleanup
 rm -f "$CMD"
