897fd8459338a67f7fd7c2f5e4d536e2a46b0cab
Georg Zachl <it@zachlge.org>
2025-07-22 18:01:41 +0200

    Move libman to bin directory, build parallel.

--- _build/images/libman/scripts/install.sh
+++ _build/images/libman/scripts/install.sh
@@ -6,9 +6,9 @@ git clone --filter=blob:none "${LIBMAN_REPO_URL}" "${LIBMAN_NAME}"
 cd "${LIBMAN_NAME}" || exit 1
 git checkout "${LIBMAN_REPO_COMMIT}"
 qmake libman.pro
-make
-mkdir -p "${TOOLS}/${LIBMAN_NAME}"
-mv libman "${TOOLS}/${LIBMAN_NAME}"
+make -j ${nproc}
+mkdir -p "${TOOLS}/${LIBMAN_NAME}/bin"
+mv libman "${TOOLS}/${LIBMAN_NAME}/bin"
 
 # Make symlinks for binaries
 cd "$TOOLS/bin" || exit
