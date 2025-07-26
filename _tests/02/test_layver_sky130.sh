c1028f71acffd5861422e7d386c38cc27f902057
Harald Pretl <harald@pretl.eu>
2025-07-26 15:51:48 +0200

    Use unique work directory for test runs

--- _tests/02/test_layver_sky130.sh
+++ _tests/02/test_layver_sky130.sh
@@ -5,17 +5,24 @@
 #
 # Test if DRC and LVS are OK on a simple inverter in sky130A.
 
+if [ -z "${RAND}" ]; then
+    RAND=$(hexdump -e '/1 "%02x"' -n4 < /dev/urandom)
+fi
+
 ERROR=0
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 RESULT=/foss/designs/runs/${RAND}/result_layver_sky130a.log
+WORKDIR=/foss/designs/runs/${RAND}
+
+mkdir -p "$WORKDIR"
 
 # Switch to sky130A PDK
 # shellcheck source=/dev/null
-source sak-pdk-script.sh sky130A > $RESULT 2>&1
+source sak-pdk-script.sh sky130A > "$RESULT" 2>&1
 # Run the DRC
-sak-drc.sh -w /tmp -b "$DIR/inv.mag"  > $RESULT 2>&1 || ERROR=1
+sak-drc.sh -w "$WORKDIR" -b "$DIR/inv.mag"  > "$RESULT" 2>&1 || ERROR=1
 # Run the LVS
-echo sak-lvs.sh -w /tmp -s "$DIR/inv.sch" -l "$DIR/inv.mag" -c inv  > $RESULT 2>&1 || ERROR=1
+echo sak-lvs.sh -w "$WORKDIR" -s "$DIR/inv.sch" -l "$DIR/inv.mag" -c inv  > "$RESULT" 2>&1 || ERROR=1
 # Check if there is an error in the log
 if [ $ERROR -eq 1 ]; then
     echo "[ERROR] Test <LayVer of sky130 inv> FAILED."
