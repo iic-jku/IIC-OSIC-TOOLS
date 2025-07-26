6e9fe73efc8ff5cce5227a50f01b33fa7fde1f0b
Harald Pretl <harald@pretl.eu>
2025-07-26 16:28:34 +0200

    Update Chisel test script to use custom run directory

--- _tests/15/test_chisel.sh
+++ _tests/15/test_chisel.sh
@@ -5,13 +5,20 @@
 #
 # Test Chisel with a simple ALU example from Martin Schoeberl
 
-TMP=$(mktemp -d)
+if [ -z "${RAND}" ]; then
+    RAND=$(hexdump -e '/1 "%02x"' -n4 < /dev/urandom)
+fi
+
+TMP=/foss/designs/runs/${RAND}
 LOG=/foss/designs/runs/${RAND}/chisel.log
 
-git clone --quiet --depth=1 https://github.com/schoeberl/chisel-examples.git $TMP
-cd $TMP || exit 1
+mkdir -p "$TMP"
 
-eval "make alu-test" &> $LOG
+git clone --quiet --depth=1 https://github.com/schoeberl/chisel-examples.git "$TMP"
+cd "$TMP" || exit 1
+
+eval "make alu-test" &> "$LOG"
+# shellcheck disable=SC2181
 if [ $? -ne 0 ]; then
     echo "[ERROR] Test <Chisel-test with ALU> FAILED. Check log <$LOG>."
     exit 1
