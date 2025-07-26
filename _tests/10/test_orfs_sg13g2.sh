2825d05677588f4c395e62227406e4010decb9d8
Harald Pretl <harald@pretl.eu>
2025-07-26 15:58:07 +0200

    Improve test script robustness and output handling

--- _tests/10/test_orfs_sg13g2.sh
+++ _tests/10/test_orfs_sg13g2.sh
@@ -6,16 +6,20 @@
 # Test if the OpenROAD flow scripts (ORFS) run successfully; we run
 # this for IHP SG13G2 only
 
-WORK_DIR=$(mktemp -d)
+if [ -z "${RAND}" ]; then
+    RAND=$(hexdump -e '/1 "%02x"' -n4 < /dev/urandom)
+fi
+
+WORK_DIR=/foss/designs/runs/${RAND}
 RESULT=/foss/designs/runs/${RAND}/result_orfs_sg13g2.log
 FLOW_HOME=$WORK_DIR/orfs/flow
 
-mkdir -p $WORK_DIR && cd $WORK_DIR || exit 1
-git clone --quiet --filter=blob:none https://github.com/The-OpenROAD-Project/OpenROAD-flow-scripts.git orfs
+mkdir -p "$WORK_DIR" && cd "$WORK_DIR" || exit 1
+git clone --quiet --filter=blob:none https://github.com/The-OpenROAD-Project/OpenROAD-flow-scripts.git orfs > /dev/null 2>&1
 cd orfs || exit 1
 ORFS_COMMIT=$(cat "$TOOLS/openroad-latest/ORFS_COMMIT")
-git checkout --quiet "$ORFS_COMMIT"
-cd $FLOW_HOME || exit 1
+git checkout --quiet "$ORFS_COMMIT" > /dev/null 2>&1
+cd "$FLOW_HOME" || exit 1
 
 # prepare environment for ORFS
 export YOSYS_EXE=$TOOLS/yosys/bin/yosys
@@ -28,7 +32,7 @@ export GDS_ALLOW_EMPTY=spi_DEF_FILL
 
 # run ORFS with IHP130 SG13G2
 export DESIGN_CONFIG=./designs/ihp-sg13g2/spi/config.mk
-make > $RESULT
+make > "$RESULT"
 
 # check if there is an error in the log
 if grep -q "ERROR" "$RESULT"; then
