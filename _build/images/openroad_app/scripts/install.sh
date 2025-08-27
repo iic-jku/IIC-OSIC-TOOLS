d123171771cfb13f7175e14a2c542d6a521b4dfd
Harald Pretl <harald.pretl@jku.at>
2025-08-27 16:19:19 +0200

    Do not link latest OpenROAD version but the one needed by LibreLane

--- _build/images/openroad_app/scripts/install.sh
+++ _build/images/openroad_app/scripts/install.sh
@@ -25,5 +25,4 @@ echo "$ORFS_COMMIT" > "${TOOLS}/${OPENROAD_APP_NAME}-latest/ORFS_COMMIT"
 
 # Make symlinks for binaries
 cd "$TOOLS/bin" || exit
-ln -s ${TOOLS}/${OPENROAD_APP_NAME}-latest/bin/* .
-
+ln -s ${TOOLS}/${OPENROAD_APP_NAME}/bin/* .
