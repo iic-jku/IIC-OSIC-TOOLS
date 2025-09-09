7b31a7c5e0eab6cd83a2efaeaf121ccefb477c72
Harald Pretl <harald.pretl@jku.at>
2025-09-09 07:55:10 +0200

    Make link creation more robust, to not link `latest` dirs (like for OpenROAD)

--- _build/images/iic-osic-tools/skel/headless/scripts/install_links.sh
+++ _build/images/iic-osic-tools/skel/headless/scripts/install_links.sh
@@ -2,19 +2,28 @@
 set -e
 
 # Create symlinks for all installed tools
-ln -s ${TOOLS}/*/bin/* ${TOOLS}/bin
+for binfile in "${TOOLS}"/*/bin/*; do
+    linkname="${TOOLS}/bin/$(basename "$binfile")"
+    # Skip if path contains "latest"
+    if [[ "$binfile" == *latest* ]]; then
+        continue
+    fi
+    if [ ! -e "$linkname" ]; then
+        ln -s "$binfile" "$linkname"
+    fi
+done
 
 # Create symlink for Xyce (lowercase)
-ln -s ${TOOLS}/bin/Xyce ${TOOLS}/bin/xyce
+ln -s "${TOOLS}"/bin/Xyce "${TOOLS}"/bin/xyce
 
 # Install wrapper for Yosys so that modules are loaded automatically
 # see https://github.com/iic-jku/IIC-OSIC-TOOLS/issues/43
-rm -f ${TOOLS}/bin/yosys
+rm -f "${TOOLS}"/bin/yosys
 # shellcheck disable=SC2016
 echo '#!/bin/bash
 if [[ $1 == "-h" ]]; then
     exec -a "$0" "$TOOLS/yosys/bin/yosys" "$@"
 else
     exec -a "$0" "$TOOLS/yosys/bin/yosys" -m ghdl -m slang "$@"
-fi' > ${TOOLS}/bin/yosys
-chmod +x ${TOOLS}/bin/yosys
+fi' > "${TOOLS}"/bin/yosys
+chmod +x "${TOOLS}"/bin/yosys
