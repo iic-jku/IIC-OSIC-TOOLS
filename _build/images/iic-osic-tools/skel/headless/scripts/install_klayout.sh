3245b28751aededab58f73caaf5e2e532b3f9e59
Patrick Fath <p_fath@hotmail.com>
2025-09-10 16:29:52 +0200

    adjusted klayout keybinding for zoom-out; added klayout auto backup plugin

--- _build/images/iic-osic-tools/skel/headless/scripts/install_klayout.sh
+++ _build/images/iic-osic-tools/skel/headless/scripts/install_klayout.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Add essential plugins to klayout: align-tool, move-tool, layer-shortcuts
+# Add essential plugins to klayout: align-tool, move-tool, layer-shortcuts, klayout-auto-backup
 git clone --depth=1 https://github.com/iic-jku/klayout-align-tool.git /headless/.klayout/salt/klayout-align-tool
 git clone --depth=1 https://github.com/iic-jku/klayout-move-tool.git /headless/.klayout/salt/klayout-move-tool
 git clone --depth=1 https://github.com/iic-jku/klayout-layer-shortcuts.git /headless/.klayout/salt/klayout-layer-shortcuts
+git clone --depth=1 https://github.com/iic-jku/klayout-auto-backup.git /headless/.klayout/salt/klayout-auto-backup
