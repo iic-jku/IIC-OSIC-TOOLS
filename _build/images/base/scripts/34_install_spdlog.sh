27b95b1352ae62690fdfe8efe80ceb6110665bc7
Harald Pretl <harald.pretl@jku.at>
2025-09-17 19:33:48 +0200

    Dial back `spdlog` version, otherwise the OpenROAD version required for LibreLane does not build anylonger

--- _build/images/base/scripts/34_install_spdlog.sh
+++ _build/images/base/scripts/34_install_spdlog.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-# Install syslog (dependency of OpenROAD)
+# Install spdlog (dependency of OpenROAD)
 SPDLOG_PREFIX="/usr/local"
-SPDLOG_VERSION=1.15.0
+SPDLOG_VERSION=1.8.1
 echo "[INFO] Installing SPDLOG version $SPDLOG_VERSION into $SPDLOG_PREFIX"
 cd /tmp || exit 1
 git clone --depth=1 -b "v${SPDLOG_VERSION}" https://github.com/gabime/spdlog.git
