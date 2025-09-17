71725585a04f488f75288c210edf4d92c9adaec3
Harald Pretl <harald.pretl@jku.at>
2025-09-17 20:24:55 +0200

    Update `spdlog` to v1.15.1 before building latest OpenROAD, otherwise build fail
    We can temporarily install a new `spdlog`, as it is not linked in the binary, just the header files are used.

--- _build/images/openroad/scripts/install.sh
+++ _build/images/openroad/scripts/install.sh
@@ -11,8 +11,20 @@ cmake .. "-DCMAKE_INSTALL_PREFIX=${TOOLS}/${OPENROAD_NAME}" "-DUSE_SYSTEM_BOOST=
 make -j"$(nproc)"
 make install
 
+# --------------------------------------------------------------
 # Build latest OpenROAD version (in addition to specified version)
-cd .. && rm -rf build && mkdir -p build && cd build
+# --------------------------------------------------------------
+# New OpenROAD needs spdlog 1.15.1, so we update it here (packaged version is 1.8.1)
+SPDLOG_PREFIX="/usr/local"
+SPDLOG_VERSION=1.15.1
+echo "[INFO] Installing SPDLOG version $SPDLOG_VERSION into $SPDLOG_PREFIX"
+cd /tmp || exit 1
+git clone --depth=1 -b "v${SPDLOG_VERSION}" https://github.com/gabime/spdlog.git
+cd spdlog || exit 1
+cmake -DCMAKE_INSTALL_PREFIX="${SPDLOG_PREFIX}" -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DSPDLOG_BUILD_EXAMPLE=OFF -B build .
+cmake --build build -j "$(nproc)" --target install
+# --------------------------------------------------------------
+cd "/tmp/${OPENROAD_NAME}" || exit 1 && rm -rf build && mkdir -p build && cd build
 git checkout -
 git submodule update --init --recursive
 cmake .. "-DCMAKE_INSTALL_PREFIX=${TOOLS}/${OPENROAD_NAME}-latest" "-DUSE_SYSTEM_BOOST=ON" "-DGTest_ROOT=/usr/local"
