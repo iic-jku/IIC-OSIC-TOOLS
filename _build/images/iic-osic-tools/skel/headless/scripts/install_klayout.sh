3a9b863d7ac8263b70c23b855599bdfae960f81f
Harald Pretl <harald.pretl@jku.at>
2025-09-17 18:44:43 +0200

    Add new plugin library, add new pin tool

--- _build/images/iic-osic-tools/skel/headless/scripts/install_klayout.sh
+++ _build/images/iic-osic-tools/skel/headless/scripts/install_klayout.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# Add essential plugins to klayout: align-tool, move-tool, layer-shortcuts, klayout-auto-backup
+# Add essential plugins to KLayout: plugin-utils, align-tool, move-tool, layer-shortcuts, klayout-auto-backup, klayout-pin-tool
+git clone --depth=1 https://github.com/iic-jku/klayout-plugin-utils.git /headless/.klayout/salt/klayout-plugin-utils
 git clone --depth=1 https://github.com/iic-jku/klayout-align-tool.git /headless/.klayout/salt/klayout-align-tool
 git clone --depth=1 https://github.com/iic-jku/klayout-move-tool.git /headless/.klayout/salt/klayout-move-tool
 git clone --depth=1 https://github.com/iic-jku/klayout-layer-shortcuts.git /headless/.klayout/salt/klayout-layer-shortcuts
 git clone --depth=1 https://github.com/iic-jku/klayout-auto-backup.git /headless/.klayout/salt/klayout-auto-backup
+git clone --depth=1 https://github.com/iic-jku/klayout-pin-tool.git /headless/.klayout/salt/klayout-pin-tool
