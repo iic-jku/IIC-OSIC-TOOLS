ea1b548d6dec64e86b390612d8897edfa3465df4
Harald Pretl <harald@pretl.eu>
2025-09-30 11:01:03 +0200

    Update KLayout manager repo in install script

--- _build/images/iic-osic-tools/skel/headless/scripts/install_klayout.sh
+++ _build/images/iic-osic-tools/skel/headless/scripts/install_klayout.sh
@@ -7,4 +7,4 @@ git clone --depth=1 https://github.com/iic-jku/klayout-move-tool.git /headless/.
 git clone --depth=1 https://github.com/iic-jku/klayout-layer-shortcuts.git /headless/.klayout/salt/klayout-layer-shortcuts
 git clone --depth=1 https://github.com/iic-jku/klayout-auto-backup.git /headless/.klayout/salt/klayout-auto-backup
 git clone --depth=1 https://github.com/iic-jku/klayout-pin-tool.git /headless/.klayout/salt/klayout-pin-tool
-git clone --depth=1 https://github.com/iic-jku/klayout-layout-manager.git /headless/.klayout/salt/klayout-layout-manager
+git clone --depth=1 https://github.com/iic-jku/klayout-library-manager.git /headless/.klayout/salt/klayout-library-manager
