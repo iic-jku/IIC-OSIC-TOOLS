0b635818d44ad6462aec873bdcf92d65314ec2e6
Harald Pretl <harald.pretl@jku.at>
2025-10-16 14:44:49 +0200

    Update gdsfactory to version 9.19.0

--- _build/images/iic-osic-tools/skel/headless/scripts/install_eda.sh
+++ _build/images/iic-osic-tools/skel/headless/scripts/install_eda.sh
@@ -10,7 +10,7 @@ pip3 install --upgrade --no-cache-dir --break-system-packages --ignore-installed
 	edalize==0.6.2 \
 	fault-dft==0.9.4 \
 	fusesoc==2.4.4 \
-	gdsfactory==9.18.1 \
+	gdsfactory==9.19.0 \
 	gdspy==1.6.13 \
 	jsonschema2md==1.7.0 \
 	klayout-pex==0.3.2 \
