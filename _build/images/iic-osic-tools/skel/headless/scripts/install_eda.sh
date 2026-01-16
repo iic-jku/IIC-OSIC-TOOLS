9838182d71d5338ef8c7a9eb2947397ceaa2fa89
Harald Pretl <harald.pretl@jku.at>
2026-01-16 22:02:57 +0100

    Update tool versions and dependencies

--- _build/images/iic-osic-tools/skel/headless/scripts/install_eda.sh
+++ _build/images/iic-osic-tools/skel/headless/scripts/install_eda.sh
@@ -16,7 +16,7 @@ pip3 install --upgrade --no-cache-dir --break-system-packages --ignore-installed
 	fault-dft==0.9.4 \
 	fusesoc==2.4.5 \
 	gds2palace==0.1.17 \
-	gdsfactory==9.30.0 \
+	gdsfactory==9.31.0 \
 	gdspy==1.6.13 \
 	gmsh==4.15.0 \
 	jsonschema2md==1.7.0 \
@@ -31,7 +31,7 @@ pip3 install --upgrade --no-cache-dir --break-system-packages --ignore-installed
 	schemdraw[svgmath]==0.21 \
 	scikit-rf==1.9.0 \
 	setupEM==0.1.21 \
-	siliconcompiler==0.36.1 \
+	siliconcompiler==0.36.2 \
 	spicelib==1.4.7 \
 	spyci==1.0.2
 
