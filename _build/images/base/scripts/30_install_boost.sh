9dd48a29f8428b126a072ddf3ee1ea0dfbf12a60
Harald Pretl <harald@pretl.eu>
2026-01-24 00:03:03 +0100

    Update Boost download to use CMake release archive

--- _build/images/base/scripts/30_install_boost.sh
+++ _build/images/base/scripts/30_install_boost.sh
@@ -9,8 +9,8 @@ BOOST_BUILD=0
 BOOST_VERSION="$BOOST_VER_MAJ.$BOOST_VER_MIN.$BOOST_BUILD"
 echo "[INFO] Installing BOOST version $BOOST_VERSION"
 cd /tmp
-wget --no-verbose https://github.com/boostorg/boost/releases/download/boost-${BOOST_VERSION}/boost-${BOOST_VERSION}.tar.gz
-tar -xf boost-${BOOST_VERSION}.tar.gz
+wget --no-verbose https://github.com/boostorg/boost/releases/download/boost-${BOOST_VERSION}/boost-${BOOST_VERSION}-cmake.tar.gz
+tar -xf boost-${BOOST_VERSION}-cmake.tar.gz
 cd boost-${BOOST_VERSION}
 ./bootstrap.sh
 ./b2 install
