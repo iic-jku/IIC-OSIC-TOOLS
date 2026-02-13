7382267d33e5a72e3bd1a8d899b83c778b7b4f37
Harald Pretl <harald.pretl@jku.at>
2026-02-13 15:24:17 -0800

    Revert to using Boost from APT

--- _build/images/base/scripts/30_install_boost.sh
+++ _build/images/base/scripts/30_install_boost.sh
@@ -3,17 +3,17 @@
 set -e
 
 # Need libboost >= 1.88 for VACASK
-BOOST_VER_MAJ=1
-BOOST_VER_MIN=88	
-BOOST_BUILD=0
-BOOST_VERSION="$BOOST_VER_MAJ.$BOOST_VER_MIN.$BOOST_BUILD"
-echo "[INFO] Installing BOOST version $BOOST_VERSION"
-cd /tmp
-wget --no-verbose https://github.com/boostorg/boost/releases/download/boost-${BOOST_VERSION}/boost-${BOOST_VERSION}-cmake.tar.gz
-tar -xf boost-${BOOST_VERSION}-cmake.tar.gz
-cd boost-${BOOST_VERSION}
-./bootstrap.sh
-./b2 install
-
-echo "[INFO] Cleaning up caches"
-rm -rf /tmp/*
+#BOOST_VER_MAJ=1
+#BOOST_VER_MIN=88	
+#BOOST_BUILD=0
+#BOOST_VERSION="$BOOST_VER_MAJ.$BOOST_VER_MIN.$BOOST_BUILD"
+#echo "[INFO] Installing BOOST version $BOOST_VERSION"
+#cd /tmp
+#wget --no-verbose https://github.com/boostorg/boost/releases/download/boost-${BOOST_VERSION}/boost-${BOOST_VERSION}.tar.gz
+#tar -xf boost-${BOOST_VERSION}.tar.gz
+#cd boost-${BOOST_VERSION}
+#./bootstrap.sh
+#./b2 install
+#
+#echo "[INFO] Cleaning up caches"
+#rm -rf /tmp/*
