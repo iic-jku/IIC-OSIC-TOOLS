dd75b49362fe9c94eacaf1f5d98f4e03532ce50f
Harald Pretl <harald.pretl@jku.at>
2026-02-13 15:29:08 -0800

    Build Boost 1.88 in vacask image install script

--- _build/images/vacask/scripts/install.sh
+++ _build/images/vacask/scripts/install.sh
@@ -2,6 +2,15 @@
 set -e
 cd /tmp || exit 1
 
+# Install custom libboost since stock libboost version is too old
+curl -LO https://archives.boost.io/release/1.88.0/source/boost_1_88_0.tar.gz
+tar xvf boost_1_88_0.tar.gz
+cd boost_1_88_0/tools/build
+./bootstrap.sh gcc
+cd ../..
+tools/build/b2 --with-filesystem --with-process --with-asio link=static toolset=gcc
+cd ..
+
 git clone --branch "${VACASK_REPO_COMMIT}" "${VACASK_REPO_URL}" "${VACASK_NAME}"
 cd "${VACASK_NAME}" || exit 1
 
