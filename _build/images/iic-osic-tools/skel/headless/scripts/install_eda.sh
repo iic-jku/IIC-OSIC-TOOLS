edb81306769a75247bfd6eee3f06e178ab533e82
Harald Pretl <harald.pretl@jku.at>
2026-02-13 20:30:35 -0800

    Restrict gmsh/gds2palace to x86_64 and quote extras

--- _build/images/iic-osic-tools/skel/headless/scripts/install_eda.sh
+++ _build/images/iic-osic-tools/skel/headless/scripts/install_eda.sh
@@ -8,17 +8,15 @@ apt install -y \
 
 echo "[INFO] Install EDA packages via PIP"
 pip3 install --upgrade --no-cache-dir --break-system-packages --ignore-installed \
-	amaranth[builtin-yosys]==0.5.6 \
+	"amaranth[builtin-yosys]==0.5.6" \
 	cace==2.9.0 \
 	ciel==2.4.0 \
 	cocotb==2.0.1 \
 	edalize==0.6.5 \
 	fault-dft==0.9.4 \
 	fusesoc==2.4.5 \
-	gds2palace==0.1.19 \
 	gdsfactory==9.34.1 \
 	gdspy==1.6.13 \
-	gmsh==4.15.0 \
 	jsonschema2md==1.7.0 \
 	klayout-pex==0.3.8 \
 	lctime==0.0.26 \
@@ -28,12 +26,19 @@ pip3 install --upgrade --no-cache-dir --break-system-packages --ignore-installed
 	pyrtl==0.12 \
 	pyuvm==4.0.1 \
 	pyverilog==1.3.0 \
-	schemdraw[svgmath]==0.21 \
+	"schemdraw[svgmath]==0.21" \
 	scikit-rf==1.10.0 \
-	setupEM==0.1.22 \
 	siliconcompiler==0.36.5 \
 	spicelib==1.4.9 \
 	spyci==1.0.2
+
+#FIXME There are currently issues with gmsh for arm64 Linux, so only install for x86_64
+if [ "$(arch)" = "x86_64" ]; then
+	pip3 install --upgrade --no-cache-dir --break-system-packages --ignore-installed \
+		gds2palace==0.1.19 \
+		gmsh==4.15.0 \
+		setupEM==0.1.22
+fi
 
 echo "[INFO] Installing CharLib"
 pip3 install --upgrade --no-cache-dir --break-system-packages --ignore-installed \
