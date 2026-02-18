3875e57a409c865ed9444783a5f7180304446a70
Harald Pretl <harald.pretl@jku.at>
2026-02-18 15:46:08 -0800

    Set default DISP to :0 in start_shell.sh
    Add a check that sets DISP=":0" when DISP is not defined, ensuring a default X display is available for container shells. This change prevents DISPLAY-related issues when launching GUI apps from the container.

--- start_shell.sh
+++ start_shell.sh
@@ -58,6 +58,10 @@ if [ -z ${CONTAINER_NAME+z} ]; then
 	CONTAINER_NAME="iic-osic-tools_shell_uid_"$(id -u)
 fi
 
+if [ -z ${DISP+z} ]; then
+	DISP=":0"
+fi
+
 # Check for UIDs and GIDs below 1000, except 0 (root)
 if [[ ${CONTAINER_USER} -ne 0 ]]  &&  [[ ${CONTAINER_USER} -lt 1000 ]]; then
 	prt_str="# [WARNING] Selected User ID ${CONTAINER_USER} is below 1000. This ID might interfere with User-IDs inside the container and cause undefined behavior! #"
