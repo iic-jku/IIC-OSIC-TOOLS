d3948ffac95ee0c876984d14c9df68cfcedf15a2
Harald Pretl <harald.pretl@jku.at>
2026-02-18 15:38:08 -0800

    Add icestorm & nextpnr sources and metadata

--- _build/images/fpga-tools/scripts/install.sh
+++ _build/images/fpga-tools/scripts/install.sh
@@ -6,8 +6,9 @@ mkdir -p "${TOOLS}/${FPGA_NAME}/bin"
 # --------------------------------
 cd /tmp || exit 1
 echo "[INFO] Installing icestorm"
-git clone --depth=1 https://github.com/YosysHQ/icestorm.git
+git clone --filter=blob:none "${ICESTORM_REPO_URL}" icestorm
 cd icestorm || exit 1
+git checkout "${ICESTORM_REPO_COMMIT}"
 PREFIX="${TOOLS}/${FPGA_NAME}" make -j"$(nproc)"
 PREFIX="${TOOLS}/${FPGA_NAME}" make install
 
@@ -15,8 +16,9 @@ PREFIX="${TOOLS}/${FPGA_NAME}" make install
 # -----------------
 cd /tmp || exit 1
 echo "[INFO] Installing nextpnr"
-git clone --depth=1 https://github.com/YosysHQ/nextpnr.git
+git clone --filter=blob:none "${NEXTPNR_REPO_URL}" nextpnr
 cd nextpnr || exit 1
+git checkout "${NEXTPNR_REPO_COMMIT}"
 git submodule update --init --recursive
 mkdir -p build && cd build || exit 1
 cmake ..    -DARCH=ice40 \
