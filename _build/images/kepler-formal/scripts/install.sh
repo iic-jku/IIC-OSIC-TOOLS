cedb04cb12446eb737600c6f268e07a939f6bed4
Harald Pretl <harald.pretl@jku.at>
2026-02-18 10:31:48 -0800

    Remove C++ standard flag and SOURCES write

--- _build/images/kepler-formal/scripts/install.sh
+++ _build/images/kepler-formal/scripts/install.sh
@@ -8,9 +8,6 @@ git checkout "${KEPLER_FORMAL_REPO_COMMIT}"
 mkdir -p build && cd build
 cmake .. \
     -DCMAKE_INSTALL_PREFIX="${TOOLS}/${KEPLER_FORMAL_NAME}" \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_CXX_STANDARD=20
+    -DCMAKE_BUILD_TYPE=Release
 make -j"$(nproc)"
 make install
-
-echo "$KEPLER_FORMAL_NAME $KEPLER_FORMAL_REPO_COMMIT" > "${TOOLS}/${KEPLER_FORMAL_NAME}/SOURCES"
