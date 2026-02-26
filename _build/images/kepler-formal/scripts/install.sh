14c4d150e159b2fc693f34d07522cf1ee0d4aa40
Harald Pretl <harald@pretl.eu>
2026-02-26 21:29:19 +0100

    Use partial clone and init submodules after checkout

--- _build/images/kepler-formal/scripts/install.sh
+++ _build/images/kepler-formal/scripts/install.sh
@@ -2,9 +2,10 @@
 set -e
 cd /tmp || exit 1
 
-git clone --recurse-submodules "${KEPLER_FORMAL_REPO_URL}" "${KEPLER_FORMAL_NAME}"
+git clone --filter=blob:none "${KEPLER_FORMAL_REPO_URL}" "${KEPLER_FORMAL_NAME}"
 cd "${KEPLER_FORMAL_NAME}" || exit 1
 git checkout "${KEPLER_FORMAL_REPO_COMMIT}"
+git submodule update --init --recursive
 mkdir -p build && cd build
 cmake .. \
     -DCMAKE_INSTALL_PREFIX="${TOOLS}/${KEPLER_FORMAL_NAME}" \
