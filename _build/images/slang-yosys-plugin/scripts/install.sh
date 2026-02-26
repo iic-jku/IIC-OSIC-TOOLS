73a84307e154ed63438dda211d2ded9930ff4864
Harald Pretl <harald@pretl.eu>
2026-02-26 21:35:48 +0100

    Enable set -e and init submodules post-checkout

--- _build/images/slang-yosys-plugin/scripts/install.sh
+++ _build/images/slang-yosys-plugin/scripts/install.sh
@@ -2,13 +2,13 @@
 #set -e
 cd /tmp || exit 1
 
-# shellcheck disable=SC2010
 export PATH="$PATH:$TOOLS/yosys/bin:$TOOLS/ghdl/bin"
 echo "[INFO] PATH=$PATH"
 
-git clone --recursive --filter=blob:none "${SLANG_YOSYS_PLUGIN_REPO_URL}" "${SLANG_YOSYS_PLUGIN_NAME}"
+git clone --filter=blob:none "${SLANG_YOSYS_PLUGIN_REPO_URL}" "${SLANG_YOSYS_PLUGIN_NAME}"
 cd "${SLANG_YOSYS_PLUGIN_NAME}" || exit 1
 git checkout "${SLANG_YOSYS_PLUGIN_REPO_COMMIT}"
+git submodule update --init --recursive
 make -j"$(nproc)"
 mkdir -p "${TOOLS}_add/yosys/share/yosys/plugins"
 cp build/slang.so "${TOOLS}_add/yosys/share/yosys/plugins"
