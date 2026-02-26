f5a60e9d979c1e889921239793eb71c50a4553e0
Harald Pretl <harald@pretl.eu>
2026-02-26 21:08:46 +0100

    Update build script header comments

--- _build/build-images.sh
+++ _build/build-images.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # ========================================================================
-# Build script for ICD@JKU docker images (build-all)
+# Build script for ICD@JKU docker images (build-images)
 #
 # SPDX-FileCopyrightText: 2022-2025 Harald Pretl and Georg Zachl
 # Johannes Kepler University, Department for Integrated Circuits
