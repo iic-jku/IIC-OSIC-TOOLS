c9a6be7dbb4ebc59a751c1e014978e1463bf2191
Harald Pretl <harald@pretl.eu>
2026-02-26 21:36:37 +0100

    Quote variables in install script

--- _build/images/vacask/scripts/install.sh
+++ _build/images/vacask/scripts/install.sh
@@ -15,9 +15,9 @@ git clone --branch "${VACASK_REPO_COMMIT}" "${VACASK_REPO_URL}" "${VACASK_NAME}"
 cd "${VACASK_NAME}" || exit 1
 
 mkdir -p build && cd build
-cmake -G Ninja -S .. -B . -DCMAKE_BUILD_TYPE=Release -DOPENVAF_DIR=${TOOLS}/openvaf/bin -DBoost_ROOT=/tmp/boost_1_88_0/stage
+cmake -G Ninja -S .. -B . -DCMAKE_BUILD_TYPE=Release -DOPENVAF_DIR="${TOOLS}/openvaf/bin" -DBoost_ROOT=/tmp/boost_1_88_0/stage
 cmake --build . -j "$(nproc)"
 cmake --install . --prefix "${TOOLS}/${VACASK_NAME}" --strip
 
 # Remove openvaf here since it is already installed with openvaf-r.
-rm -rf ${TOOLS}/${VACASK_NAME}/bin/openvaf-r
+rm -rf "${TOOLS}/${VACASK_NAME}/bin/openvaf-r"
