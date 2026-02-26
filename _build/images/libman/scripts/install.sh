3235de63bebfe02a652de12d0c37a7682bf59c70
Harald Pretl <harald@pretl.eu>
2026-02-26 21:29:36 +0100

    Use $(nproc) in make -j invocation

--- _build/images/libman/scripts/install.sh
+++ _build/images/libman/scripts/install.sh
@@ -6,6 +6,6 @@ git clone --filter=blob:none "${LIBMAN_REPO_URL}" "${LIBMAN_NAME}"
 cd "${LIBMAN_NAME}" || exit 1
 git checkout "${LIBMAN_REPO_COMMIT}"
 qmake6 libman.pro
-make -j ${nproc}
+make -j"$(nproc)"
 mkdir -p "${TOOLS}/${LIBMAN_NAME}/bin"
 mv libman "${TOOLS}/${LIBMAN_NAME}/bin"
