459c84c3432c7b7d25ead4dff4b90ee3c9296e0e
Harald Pretl <harald@pretl.eu>
2026-03-07 10:56:13 +0100

    Bump tool repo commits & gdsfactory to 9.39.0

--- _build/images/iic-osic-tools/skel/headless/scripts/install_eda.sh
+++ _build/images/iic-osic-tools/skel/headless/scripts/install_eda.sh
@@ -19,7 +19,7 @@ pip3 install $PIP_FLAGS \
 	edalize==0.6.5 \
 	fault-dft==0.9.4 \
 	fusesoc==2.4.5 \
-	gdsfactory==9.38.0 \
+	gdsfactory==9.39.0 \
 	gdspy==1.6.13 \
 	jsonschema2md==1.7.0 \
 	klayout-pex==0.3.9 \
