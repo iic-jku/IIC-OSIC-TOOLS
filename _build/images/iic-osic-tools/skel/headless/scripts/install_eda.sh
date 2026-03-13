173731a7bda745c2b65ddbc59d7c287652f1d5e4
Harald Pretl <harald.pretl@jku.at>
2026-03-13 14:17:34 +0100

    Bump tool repo commits and package versions

--- _build/images/iic-osic-tools/skel/headless/scripts/install_eda.sh
+++ _build/images/iic-osic-tools/skel/headless/scripts/install_eda.sh
@@ -25,7 +25,7 @@ pip3 install $PIP_FLAGS \
 	gdspy==1.6.13 \
 	jsonschema2md==1.7.0 \
 	klayout-pex==0.3.9 \
-    klayout-vector-file-export-cli==0.1 \
+    klayout-vector-file-export-cli==0.4 \
 	lctime==0.0.26 \
 	librelane==2.4.13 \
 	najaeda==0.5.0 \
