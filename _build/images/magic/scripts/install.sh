e43b6a4c70744ffd45ac2306724fe6b1be7ccd1c
Harald Pretl <harald.pretl@jku.at>
2026-03-25 20:56:39 +0100

    Revert "Workaround upstream bugs in Magic install script"
    This reverts commit c484a6c300a6b54ae76d1df199c2b53176ceb436.

--- _build/images/magic/scripts/install.sh
+++ _build/images/magic/scripts/install.sh
@@ -5,13 +5,6 @@ cd /tmp || exit 1
 git clone --filter=blob:none "${MAGIC_REPO_URL}" "${MAGIC_NAME}"
 cd "${MAGIC_NAME}" || exit 1
 git checkout "${MAGIC_REPO_COMMIT}"
-
-# FIXME Fix upstream bugs in 8.3.627: unclosed comment hiding DRC_EXCEPTION_MASK
-# FIXME definition and extra closing parentheses in DRCbasic.c
-sed -i 's|an exception or an exemption.|an exception or an exemption. */|' drc/drc.h
-sed -i 's|MASK) == 0)))|MASK) == 0))|g' drc/DRCbasic.c
-sed -i 's|MASK) == 1)))|MASK) == 1))|g' drc/DRCbasic.c
-
 ./configure --prefix="${TOOLS}/${MAGIC_NAME}"
 make database/database.h
 make -j"$(nproc)"
