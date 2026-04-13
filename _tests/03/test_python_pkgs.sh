2456508337e544b16b21e8675b94714f4f7aec5b
Harald Pretl <harald.pretl@jku.at>
2026-04-13 11:44:25 +0200

    Update Python package tests and imports

--- _tests/03/test_python_pkgs.sh
+++ _tests/03/test_python_pkgs.sh
@@ -6,12 +6,33 @@
 # Test if a few of the import Python packages work properly.
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ERR=0
 
 if ! python "$DIR/pkgs.py" 
 then
     echo "[ERROR] Test <Loading Python-packages> FAILED."
-    exit 1
+    ERR=1
 else
     echo "[INFO] Test <Loading Python-packages> passed."
-    exit 0
 fi
+
+if ! /foss/tools/charlib/bin/python -c "import charlib"
+then
+    echo "[ERROR] Test <Loading charlib> FAILED."
+    ERR=1
+else
+    echo "[INFO] Test <Loading charlib> passed."
+fi
+
+if ! /foss/tools/vlsirtools/bin/python -c "import hdl21"
+then
+    echo "[ERROR] Test <Loading hdl21> FAILED."
+    ERR=1
+else
+    echo "[INFO] Test <Loading hdl21> passed."
+fi
+
+# Clean up log file created by najaeda import
+rm -f naja_perf.log
+
+exit $ERR
