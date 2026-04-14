824594847f5c6d2435cbe58389351532f0bccae3
Harald Pretl <harald.pretl@jku.at>
2026-04-14 21:57:19 +0200

    Update tool commits and bump gdsfactory

--- _build/images/iic-osic-tools/skel/headless/scripts/install_eda.sh
+++ _build/images/iic-osic-tools/skel/headless/scripts/install_eda.sh
@@ -26,7 +26,7 @@ pip3 install $PIP_FLAGS \
 	edalize==0.6.6 \
 	fault-dft==0.9.4 \
 	fusesoc==2.4.5 \
-	gdsfactory==9.40.0 \
+	gdsfactory==9.40.1 \
 	gdsfill==0.1.5 \
 	gdspy==1.6.13 \
 	jsonschema2md==1.7.0 \
