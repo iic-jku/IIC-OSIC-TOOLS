453f5d889ec0329d052e87a8b62cfd2075bc7167
Harald Pretl <harald.pretl@jku.at>
2026-05-13 18:13:31 +0100

    Safer permission handling in set_user_permission

--- _build/images/base/skel/dockerstartup/scripts/set_user_permission.sh
+++ _build/images/base/skel/dockerstartup/scripts/set_user_permission.sh
@@ -4,17 +4,31 @@
 # SPDX-License-Identifier: Apache-2.0
 ### every exit != 0 fails the script
 set -e
-if [[ -n $DEBUG ]]; then
-    verbose="-v"
-fi
+
+# -v only when DEBUG is set; safe under `set -u`
+verbose="${DEBUG:+-v}"
 
 for var in "$@"
 do
+    if [ ! -d "$var" ]; then
+        echo "[WARN] Skipping non-directory: $var"
+        continue
+    fi
+
     [ -z "${IIC_OSIC_TOOLS_QUIET}" ] && echo "[INFO] Fix permissions for: $var"
+
+    # Only act on regular files / directories; never follow symlinks (would
+    # otherwise modify permissions/ownership outside of "$var").
     # shellcheck disable=SC2086
-    find "$var"/ -name '*.sh' -exec chmod $verbose a+x {} +
+    find "$var" -type f -name '*.sh'      -exec chmod $verbose a+x {} +
     # shellcheck disable=SC2086
-    find "$var"/ -name '*.desktop' -exec chmod $verbose a+x {} +
+    find "$var" -type f -name '*.desktop' -exec chmod $verbose a+x {} +
+
+    # chgrp -h on symlinks themselves, recurse without following links.
+    find "$var" \( -type f -o -type d -o -type l \) -exec chgrp $verbose -h 0 {} +
+
     # shellcheck disable=SC2086
-    chgrp -R 0 "$var" && chmod -R $verbose a+rw "$var" && find "$var" -type d -exec chmod $verbose a+x {} +
+    find "$var" -type d -exec chmod $verbose a+rwx {} +
+    # shellcheck disable=SC2086
+    find "$var" -type f -exec chmod $verbose a+rw  {} +
 done
