0313bb136d6925b9d5f4423412739ae2ff13d1cf
Simon Dorrer <simon.dorrer@jku.at>
2026-06-18 00:13:07 +0200

    updated `sak-pex.sh` - resolve `-w` with `realpath -m`

--- _build/images/iic-osic-tools/skel/foss/tools/sak/sak-pex.sh
+++ _build/images/iic-osic-tools/skel/foss/tools/sak/sak-pex.sh
@@ -108,7 +108,8 @@ while getopts "m:s:w:n:t:r:y:d" flag; do
 			;;
 		w)
 			[ $DEBUG -eq 1 ] && echo "[INFO] Flag -w is set to <$OPTARG>."
-			RESDIR=$(realpath "$OPTARG")
+			# -m so a not-yet-existing (multi-level) workdir still resolves. It is created below.
+			RESDIR=$(realpath -m "$OPTARG")
 			;;
 		n)
 			[ $DEBUG -eq 1 ] && echo "[INFO] Flag -n is set to <$OPTARG>."
